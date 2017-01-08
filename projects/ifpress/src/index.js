f3176c5e15c0766f5328d91877fd8891b9388e9f
chicagodave <Ah$3tub!>
2017-01-07 18:52:58 -0600

    One more lost file and one moved file.

---
+++ projects/ifpress/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Story from './templates/standard/Story';
+import './templates/standard/index.css';
+import '../semantic/dist/semantic.min.css';
+
+ReactDOM.render(
+  <Story />,
+  document.getElementById('root')
+);
