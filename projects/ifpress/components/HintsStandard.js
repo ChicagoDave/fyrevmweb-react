07577a8ee4e6cf2d2d230d105e389ce4d857187a
chicagodave <Ah$3tub!>
2017-01-07 18:51:09 -0600

    Fixed lost files. Weird.

---
+++ projects/ifpress/components/HintsStandard.js
@@ -0,0 +1,6 @@
+/**
+ * Created by david.cornelson on 12/29/2016.
+ *
+ * Slide out panel from right taking up 30% of screen below menu
+ *
+ */
---
+++ projects/ifpress/components/StandardMenu.js
@@ -0,0 +1,6 @@
+/**
+ * Created by david.cornelson on 12/30/2016.
+ *
+ * Should contain story.title
+ *
+ */
