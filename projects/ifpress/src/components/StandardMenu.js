e06776a2f8e407efe9e7fd6afc1edc67a5bd9511
Nick Tzaperas <nick@nijotz.com>
2017-01-08 16:32:48 -0800

    Fix warnings

--- projects/ifpress/src/components/StandardMenu.js
+++ projects/ifpress/src/components/StandardMenu.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import { Menu } from 'semantic-ui-react'
 
 export default class StatusLine extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <Menu/>
--- projects/ifpress/src/templates/standard/Story.jsx
+++ projects/ifpress/src/templates/standard/Story.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Header, Input, Grid, Menu } from 'semantic-ui-react'
+import { Grid } from 'semantic-ui-react'
 import { StandardMenu } from '../../components/StandardMenu.js'
 import { StatusLine } from '../../components/StandardStatusLine.js'
 import { ScrollingContent } from '../../components/ScrollingContent.js'
@@ -27,12 +27,11 @@ class Story extends Component {
     }
 
     render() {
-
         return (
             <div>
                 <StandardMenu props="{this.state}"/>
                 <Grid divided id='story'>
-                    <StatusLine props={this.fyrevm}/>
+                    <StatusLine props="{this.fyrevm}"/>
                     <ScrollingContent props="{this.fyrevm}"/>
                 </Grid>
             </div>
