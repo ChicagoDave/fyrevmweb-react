1584343aa8d022eb590de7dd23e770b4e1637d5e
Nick Tzaperas <nick@nijotz.com>
2017-01-08 17:11:44 -0800

    Put something back on the menu

--- projects/ifpress/src/components/StandardMenu.js
+++ projects/ifpress/src/components/StandardMenu.js
@@ -4,7 +4,11 @@ import { Menu } from 'semantic-ui-react'
 export default class StatusLine extends Component {
     render() {
         return (
-            <Menu/>
+            <Menu>
+                <Menu.Item>
+                    FyreVM Prototype
+                </Menu.Item>
+            </Menu>
         );
     }
 }
