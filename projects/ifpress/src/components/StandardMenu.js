52159b223d08cb536a481274edc9b910128b753d
david.cornelson <david.cornelson@accenture.com>
2017-01-11 15:52:43 -0600

    Re-added FyreVMWeb.js to public and added menu items.

--- projects/ifpress/src/components/StandardMenu.js
+++ projects/ifpress/src/components/StandardMenu.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import { Menu } from 'semantic-ui-react'
+import { Dropdown, Icon, Input, Menu } from 'semantic-ui-react'
 
 export default class StatusLine extends Component {
+    state = {}
+
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
     render() {
+        const { activeItem } = this.state
+
         return (
             <Menu>
-                <Menu.Item>
-                    FyreVM Prototype
-                </Menu.Item>
+                <Menu.Item header>FyreVM Prototype</Menu.Item>
+                <Dropdown as={Menu.Item} text='Stories'>
+                    <Dropdown.Menu>
+                        <Dropdown.Item text='Install New Story' />
+                        <Dropdown.Item text='Save Story to File' />
+                        <Dropdown.Item text='Start Story' />
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Menu.Item name='genhelp' active={activeItem === 'genhelp'} onClick={this.handleItemClick}>Help</Menu.Item>
+                <Menu.Item name='storyhelp' active={activeItem === 'storyhelp'} onClick={this.handleItemClick}>Story Help</Menu.Item>
+                <Menu.Item name='storyhints' active={activeItem === 'storyhints'} onClick={this.handleItemClick}>Story Hints</Menu.Item>
+                <Menu.Item name='about' active={activeItem === 'about'} onClick={this.handleItemClick}>About</Menu.Item>
             </Menu>
         );
     }
