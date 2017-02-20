ba87504bf3361deb670abe43fe3f9b1f2804b3e3
Nick Tzaperas <nick@nijotz.com>
2017-02-20 11:39:43 -0800

    Don't compile all of glulx-typescript

--- src/components/StandardMenu.js
+++ src/components/StandardMenu.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Dropdown, Icon, Input, Menu } from 'semantic-ui-react'
+import { Dropdown, Menu } from 'semantic-ui-react'
 
 export default class StatusLine extends Component {
     state = {}
